d45c03b5842e9a8bf79c2aee661f4e9fc792f259
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-04-15 07:36:33 +0200

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Umair Salam on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
